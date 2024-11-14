1718f8ab89f2da05b84b9d024ad89957399a8b5e
ysanchez_home <yagosguzman@gmail.com>
2024-11-14 21:59:22 +0100

    WIP ex01

---
+++ ex01/Data.hpp
@@ -0,0 +1,14 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <iostream>
+
+typedef struct Data
+{
+	int i;
+	double d;
+	char c;
+	std::string str;
+};
+
+#endif
---
+++ ex01/Serializer.hpp
@@ -0,0 +1,20 @@
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+#include <iostream>
+#include "Data.hpp"
+
+class Serializer
+{
+private:
+	Serializer();
+	Serializer(const Serializer& copy);
+	Serializer& operator=(const Serializer& src);
+	~Serializer();
+public:
+	static uintptr_t serialize(Data* ptr);
+	static Data* deserialize(uintptr_t raw);
+
+};
+
+#endif
